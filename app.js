24a9bc9ae617b263a0dffb59083cb43c57ed4260
penaone <penaone@gmail.com>
2020-09-22 18:29:06 -0700

    package-lock.json

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// const generatePage = () => 'Name: Jane, Github: janehub';
+// console.log(generatePage());
+// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name, github] = profileDataArgs;
+// const generatePage = (userName, githubName) => {
+//   return `
+//     Name: ${userName}
+//     GitHub: ${githubName}
+//   `;
+// };
+// console.log(name, github);
+// console.log(generatePage(name, github));
+const generatePage = (name, github) => {
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
 
-const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length;  i +=1) {
-    console.log(profileDataArr[i]);
-  }
-  };
-  console.log('=============');
-
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
+};
+// console.log(name, github);
+console.log(generatePage('name', 'github'));
