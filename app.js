f216689fc174f0fb9ce8e5e3eac416797fae3ae0
penaone <penaone@gmail.com>
2020-09-23 18:23:18 -0700

    added inquirer

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-// const generatePage = () => 'Name: Jane, Github: janehub';
-// console.log(generatePage());
-// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// const [name, github] = profileDataArgs;
-// const generatePage = (userName, githubName) => {
-//   return `
-//     Name: ${userName}
-//     GitHub: ${githubName}
-//   `;
-// };
-// console.log(name, github);
-// console.log(generatePage(name, github));
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
 
-const [name, github] = profileDataArgs;
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
-  console.log('Portfolio complete! Check out index.html to see the output!')
-});
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
